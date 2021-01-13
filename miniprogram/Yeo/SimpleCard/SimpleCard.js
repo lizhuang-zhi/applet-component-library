193271140361c466b303565ec2c3b8c9a1dedccd
Mr.KLeo <2315831906@qq.com>
2021-01-13 17:25:37 +0800

    simple-card组件开发

---
+++ miniprogram/Yeo/SimpleCard/SimpleCard.js
@@ -0,0 +1,42 @@
+// Yeo/SimpleCard/SimpleCard.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 图片
+    ImgUrl: {
+      type: String,
+      value: '../imgs/introduce.jpg'
+    },
+    // 标题
+    title: {
+      type: String,
+      value: '云顶犀开发社区'
+    },
+    // 内容
+    description: {
+      type: String,
+      value: '致力于打造多元化生态系统'
+    },
+    // 背景颜色
+    BackgroundColor: {
+      type: String,
+      value: 'orange'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
