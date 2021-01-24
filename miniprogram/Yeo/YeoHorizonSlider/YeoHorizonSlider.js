0344347da150f80c7fb945b8c8115461039739f7
Mr.KLeo <2315831906@qq.com>
2021-01-24 14:55:00 +0800

    横向滑动栏yeo-horizon-slider

---
+++ miniprogram/Yeo/YeoHorizonSlider/YeoHorizonSlider.js
@@ -0,0 +1,27 @@
+// Yeo/YeoHorizonSlider/YeoHorizonSlider.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 遍历数组数据
+    sliderArr: {
+      type: Array,
+      value: null
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/yeo-horizon-slider/yeo-horizon-slider.js
@@ -0,0 +1,71 @@
+// miniprogram/pages/yeo-horizon-slider/yeo-horizon-slider.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sliderArr: [
+      {picUrl: '../imgs/YHS-1.jpg',title: '怦然心动',description: '月亮将会奔你而来'},
+      {picUrl: '../imgs/YHS-2.jpg',title: '傲慢与偏见',description: '以灵魂唤醒灵魂'},
+      {picUrl: '../imgs/YHS-3.jfif',title: '老人与海',description: '老人与海的固执'},
+      {picUrl: '../imgs/YHS-4.jpg',title: '复仇者联盟',description: '以父之名论英雄'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
