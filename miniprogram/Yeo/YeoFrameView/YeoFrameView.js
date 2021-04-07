1a3710522fc268d8d1caed4fe8515bf02c98e85a
Mr.KLeo <2315831906@qq.com>
2021-04-07 09:27:44 +0800

    yeo统一框架设计优化完成

--- miniprogram/Yeo/YeoFrameView/YeoFrameView.js
+++ miniprogram/Yeo/YeoFrameView/YeoFrameView.js
@@ -22,14 +22,50 @@ Component({
     // 框架内边距
     framePadding: {
       type: String,
-      value: '10rpx'
+      value: '0rpx'
     },
     // 框架外边距
     frameMargin: {
       type: String,
       value: '0rpx'
-    } 
+    },
+    // 框架圆角
+    frameBordRadius: {
+      type: String,
+      value: '20rpx'
+    },
+    // 框架背景色
+    frameBgColor: {
+      type: String,
+      value: 'green'
+    },
+    // 框架box-sizing（content-box、border-box）
+    frameBoxSizing: {
+      type: String,
+      value: 'content-box'
+    },
+    // 框架align-items 
+    frameAlignItems: {
+      type: String,
+      value: 'stretch'
+    }
     
+  },
+
+  lifetimes: {
+    attached() {
+      /* 
+        初始化行列背景色（易于区分）
+      */
+      // 获取行列信息
+      let RowOrCol = this.properties.direction;
+      if(RowOrCol == 'row') {
+        this.setData({
+          frameBgColor: 'red'
+        })
+      }
+
+    }
   },
 
   /**
